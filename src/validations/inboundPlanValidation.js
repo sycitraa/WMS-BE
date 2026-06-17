28b621627dc62e301a370cd4f649aead5106ad57
Mahesa Rahmad Ramdani <mahesarahmad7@student.uns.ac.id>
2026-06-17 12:34:23 +0700

    fix

--- src/validations/inboundPlanValidation.js
+++ src/validations/inboundPlanValidation.js
@@ -19,7 +19,8 @@ const updateInboundPlanSchema = Joi.object({
 });
 
 const updateInboundPlanStatusSchema = Joi.object({
-  status: Joi.string().valid('APPROVE', 'REJECT').required()
+  status: Joi.string().valid('APPROVE', 'REJECT').required(),
+  remarks: Joi.string().allow('', null).optional()
 });
 
 module.exports = {
